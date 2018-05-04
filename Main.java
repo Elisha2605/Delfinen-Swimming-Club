718850e19641e76cbc106c2942919b6aa4e71533
Elisha <alvinjohnson@Elishas-MacBook-Pro.local>
2018-05-04 13:55:45 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+
+   
+
+   public static void main(String[] args){
+       MembersList memberList = new MembersList();
+   
+       memberList.printMember();
+   
+   }
+   
+      
+     
+}
